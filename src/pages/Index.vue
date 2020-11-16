c06eb27049691aeb3ae717cad5ddfcdc7c29cd5c
Yao Ran <7833754+shukuang_yao@user.noreply.gitee.com>
2020-11-16 14:29:11 +0800

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,110 @@
+<template>
+  <Layout>
+  
+  <!-- Page Header -->
+  <header 
+    class="masthead" 
+    :style="{backgroundImage: `url(${Gridsome_Base_Url + general.cover.url})`}"
+    >
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>{{general.title}}</h1>
+            <span class="subheading">{{general.subtitle}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
+          <g-link :to="'/post/'+edge.node.id">
+            <h2 class="post-title">
+              {{edge.node.title}}
+            </h2>
+            <!-- <h3 class="post-subtitle">
+              Problems look mighty small from 150 miles up
+            </h3> -->
+          </g-link>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            {{edge.node.created_at}}
+          </p>
+          <div>
+            <span v-for="tag in edge.node.tags" :key="tag.id">
+              <g-link :to="'/tag/'+tag.id">{{tag.title}}&nbsp;&nbsp;</g-link>
+            </span>
+          </div>
+          <hr>
+        </div>
+
+        <!-- Pager -->
+        <Pager :info="$page.posts.pageInfo"/>
+      </div>
+    </div>
+  </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($page: Int) {
+  posts: allStrapiPost(perPage: 2, page: $page) @paginate{
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges{
+      node{
+        id
+        title
+        content
+        created_at
+        tags{
+          id
+          title
+        }
+      }
+    }
+  }
+  
+  general:allStrapiGeneral{
+    edges{
+      node{
+        id
+        title
+        subtitle
+        cover{
+          url
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import { Pager } from 'gridsome'
+export default {
+  name: 'HomePage',
+  components: {
+    Pager
+  },
+  computed: {
+    general(){
+      return this.$page.general.edges[0].node
+    }
+  },
+  created () {
+    console.log('this.Gridsome_Base_Url', this.Gridsome_Base_Url)
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/templates/Tag.vue
@@ -0,0 +1,65 @@
+<template>
+  <Layout>
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{ $page.tag.title }}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div
+            class="post-preview"
+            v-for="post in $page.tag.posts"
+            :key="post.id"
+          >
+            <g-link :to="'/post/' + post.id">
+              <h2 class="post-title">
+                {{ post.title }}
+              </h2>
+              <!-- <h3 class="post-subtitle">
+              Problems look mighty small from 150 miles up
+            </h3> -->
+            </g-link>
+            <p class="post-meta">
+              Posted by
+              <a href="#">Start Bootstrap</a>
+              {{ post.created_at }}
+            </p>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+<page-query>
+query($id: ID!){
+  tag:strapiTag(id:$id){
+    id
+    title
+    posts{
+      id
+      title
+      content
+      created_at
+    }
+  }
+}
+</page-query>
+<script>
+export default {
+  name: "TagPage",
+};
+</script>
+
+<style>
+</style>
